c612ab60ba82e1d1bd7dfe2eb9f716fe1bc3c206
Yazmin <118831153+YazminFerreyra23@users.noreply.github.com>
2024-05-18 11:37:28 -0300

    logo

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,14 +1,14 @@
-document.addEventListener("DOMContentLoaded", function() {
-    fetch("/partials/navbar.html")
-        .then(response => response.text())
-        .then(data => {
-            console.log("Disparé"); // Este console.log se ejecutará después de que la respuesta del fetch sea exitosa
-            document.getElementById("navbarContainer").innerHTML = data;
-        })
-        .catch(error => {
-            console.error('Error loading navbar:', error); // Este console.log se ejecutará si hay un error en el fetch
-            console.log("Disparé dentro del catch"); // Este console.log se ejecutará si hay un error en el fetch
-        });
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("/partials/navbar.html")
+    .then((response) => response.text())
+    .then((data) => {
+      console.log("Disparé"); // Este console.log se ejecutará después de que la respuesta del fetch sea exitosa
+      document.getElementById("navbarContainer").innerHTML = data;
+    })
+    .catch((error) => {
+      console.error("Error loading navbar:", error); // Este console.log se ejecutará si hay un error en el fetch
+      console.log("Disparé dentro del catch"); // Este console.log se ejecutará si hay un error en el fetch
+    });
 });
- 
-fetch(api['cardLibro'])
+
+fetch(api["cardLibro"]);
